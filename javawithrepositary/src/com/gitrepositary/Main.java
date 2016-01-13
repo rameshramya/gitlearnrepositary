864b6c2d4761f50fa19cc71940bc1a29ee890967
RamyaRamesh <friendramya2003@gmail.com>
2016-01-13 10:01:43 -0500

    Initial commit

---
+++ javawithrepositary/src/com/gitrepositary/Main.java
@@ -0,0 +1,18 @@
+package com.gitrepositary;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			TestData  data = new TestData();
+			displayText();
+			data.displayMessage();
+	}
+	
+	public static void displayText()
+	{
+		System.out.println("Welcome 111111111");
+		System.out.println("Welcome 222222222");
+	}
+
+}
---
+++ javawithrepositary/src/com/gitrepositary/TestData.java
@@ -0,0 +1,9 @@
+package com.gitrepositary;
+
+public class TestData {
+
+	public void displayMessage()
+	{
+		System.out.println("Display 33333333333");
+	}
+}
